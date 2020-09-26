157d045a7ba416d7f06d65d0e36d29f6369ed5dc
Yasmin van Dijk <vandijk.yasmin@gmail.com>
2020-09-26 17:01:31 +0200

    tree_grow_b functie

---
+++ tree_grow_b.R
@@ -0,0 +1,29 @@
+# tree grow function with bagging
+tree_grow_b <- function(x, y, nmin, minleaf, nfeat, m) {
+  # combine features with classes
+  cases <- x
+  cases$class <- y
+  
+  # create a list for the trees
+  tree_list <- list()
+  
+  # create m trees, each grown on a bootstrap sample of x
+  for (i in 1:m) {
+    # take a random sample
+    sample_cases <- cases[sample(nrow(cases), (nrow(cases)), replace = TRUE), ]
+    
+    attributes <- cases[-ncol(cases)]
+    classes <- cases[ncol(cases)]
+    
+    # grow a tree for this sample
+    tree <- tree_grow(attributes, classes, nmin, minleaf, nfeat)
+    
+    tree_list[[i]] <- tree
+  }
+  
+  return (tree_list)
+}
+
+tr <- tree_grow_b(attributes, classes, 2, 1, ncol(attributes), 10)
+
+print(tr)
